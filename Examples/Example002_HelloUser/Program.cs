3b87b8ac40e5fe99248e43d96e659b325524d948
Elena <osinnikovaelena@mail.ru>
2022-06-26 16:59:34 +0300

    Example002.Gitignor

---
+++ Examples/Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿//WriteLine-вывод текста и перенос на новую строку
+//Write- вывод текста
+Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
